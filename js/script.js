c026c5f9833e789540f44231a97b3d54ccb88a3b
Ayomide <ayomidedans@gmail.com>
2022-02-05 11:38:56 +0100

    finale

--- js/script.js
+++ js/script.js
@@ -22,3 +22,46 @@ var swiper = new Swiper(".home-slider", {
     clickable: true,
   },
 });
+
+var swiper = new Swiper(".review-slider", {
+  spaceBetween: 20,
+  grabCursor: true,
+  loop: true,
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    600: {
+      slidesPerView: 2,
+    },
+  },
+});
+
+var swiper = new Swiper(".blogs-slider", {
+  spaceBetween: 20,
+  grabCursor: true,
+  loop: true,
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    991: {
+      slidesPerView: 3,
+    },
+  },
+});
